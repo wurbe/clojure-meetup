edb18c488b40a3bdef67d1b42acedba7be72e48f
Mihai Soloi <soloi@eloquentix.com>
2013-12-17 18:15:13 +0200

    added midje plugin and gitignore file

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject quux "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  :profiles {:dev
+                {
+                  :dependencies [[midje "1.5.0"]],
+                  :plugins [[lein-midje "3.1.1"]]
+                }
+             }
+)
